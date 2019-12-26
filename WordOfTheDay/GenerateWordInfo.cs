2ba29976ba4fc95dcda4d34d1531403e52557646
Joe Davis <davisj38@michigan.gov>
2019-12-26 11:04:42 -0500

    Initial file upload

---
+++ WordOfTheDay/GenerateWordInfo.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace WordOfTheDay
+{
+    class GenerateWordInfo
+    {
+        private static int GetRandNum()
+        {
+            Random rnd = new Random();
+            int rndNum = rnd.Next(370100);
+
+            while (ChkRandNum(rndNum) == true)
+            {
+                rndNum = rnd.Next(370100);
+            }
+
+            return rndNum;
+        }
+
+        private static bool ChkRandNum(int num)
+        {
+            bool numIsUsed = false;
+        
+            string line;
+
+            System.IO.StreamReader file = new System.IO.StreamReader(
+                System.IO.Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory
+                ) + @"\words_used.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line == num.ToString())
+                {
+                    numIsUsed = true;
+                }
+            }
+
+            file.Close();
+
+            return numIsUsed;
+        }
+        
+        private static void RemUsedNum(int usedNum)
+        {
+            string path = (System.AppDomain.CurrentDomain.BaseDirectory
+                ) + @"\words_used.txt";
+
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(usedNum);
+            }
+        }
+
+        public static Tuple<string, string, string, string, string> GetWord()
+        {
+            FINDWORD:;
+
+            int rndNum = GetRandNum();
+            int lineCount = 0;
+            string lineWord;
+            
+            System.IO.StreamReader file = new System.IO.StreamReader(
+                System.IO.Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory
+                ) + @"\words_alpha.txt");
+            while ((lineWord = file.ReadLine()) != null)
+            {
+                if (lineCount == rndNum)
+                {
+                    //System.Console.WriteLine(lineWord);
+                    break;
+                }
+
+                lineCount++;
+            }
+
+            file.Close();
+
+            var wordData = new Tuple<string, string, string, string, string>("", "", "", "", "");
+
+            wordData =  GetWordData(lineWord, rndNum);
+
+            if (wordData.Item4 == "-empty-")
+            {
+                goto FINDWORD;
+            }
+            
+            return wordData;
+        }
+        private static Tuple<string, string, string, string, string> GetWordData(string word, int rndNum)
+        {
+            string pronounce = "";
+            string nounVerbAdj = "";
+            string defOne = "";
+            string defTwo = "";
+
+            string responseFromServer = "";
+
+            HttpWebRequest wordReq = (HttpWebRequest)WebRequest.Create(
+                "https://www.dictionaryapi.com/api/v1/references/collegiate/xml/" + 
+                word + "?key=68ac85aa-372d-47c2-9515-e2e456cb3374"
+                );
+
+            WebResponse wordRes = wordReq.GetResponse();
+
+            if (((HttpWebResponse)wordRes).StatusDescription == "OK")
+            {
+                Stream dataStream = wordRes.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+                responseFromServer = reader.ReadToEnd();
+
+                reader.Close();
+            }
+
+            wordRes.Close();
+
+            if ((responseFromServer.IndexOf("suggestion") != -1) || (responseFromServer == "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\r\n<entry_list version=\"1.0\">\n\t</entry_list>"))
+            {
+                RemUsedNum(rndNum);
+                Console.WriteLine("Bad Number: " + rndNum);
+            }
+
+            XmlDocument doc = new XmlDocument();
+            doc.PreserveWhitespace = true;
+            try
+            {
+                doc.LoadXml(responseFromServer);
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                Console.Out.WriteLine("FileNotFoundException");
+            }
+
+            doc.LoadXml(responseFromServer);
+
+            XmlNamespaceManager nsmgr = new XmlNamespaceManager(doc.NameTable);
+
+            XmlElement root = doc.DocumentElement;
+            XmlNodeList entryList = root.SelectNodes("/entry_list/entry", nsmgr);
+            XmlNode entry = entryList[0];
+
+            try
+            {
+                word = entry.InnerXml.Split(new string[] { "<ew" }, StringSplitOptions.None)[1].Split(new string[] { "</ew" }, StringSplitOptions.None)[0].Trim()
+                    .Replace("<fw>", "").Replace("</fw>", "").Replace("<sx>", "").Replace("</sx>", "")
+                    .Replace("<vi>", "").Replace("</vi>", "").Replace("<it>", "").Replace("</it>", "")
+                    .Replace(">", "")
+                    .Replace("hindex=\"1\"", "").Replace("hindex=\"2\"", "")
+                    .Replace("hindex=\"3\"", "").Replace("hindex=\"4\"", "")
+                    .Replace("hindex=\"5\"", "").Replace("hindex=\"6\"", "");
+                word = word.First().ToString().ToUpper() + word.Substring(1);
+            }
+            catch (Exception) { word = "-empty-"; }
+
+            try
+            {
+                pronounce = entry.InnerXml.Split(new string[] { "<hw" }, StringSplitOptions.None)[1].Split(new string[] { "</hw" }, StringSplitOptions.None)[0].Trim()
+                    .Replace("<fw>", "").Replace("</fw>", "").Replace("<sx>", "").Replace("</sx>", "")
+                    .Replace("<vi>", "").Replace("</vi>", "").Replace("<it>", "").Replace("</it>", "")
+                    .Replace(">", "")
+                    .Replace("hindex=\"1\"", "").Replace("hindex=\"2\"", "")
+                    .Replace("hindex=\"3\"", "").Replace("hindex=\"4\"", "")
+                    .Replace("hindex=\"5\"", "").Replace("hindex=\"6\"", "");
+                pronounce = pronounce.First().ToString().ToUpper() + pronounce.Substring(1);
+            }
+            catch (Exception) { pronounce = "-empty-"; }
+
+            try
+            {
+                nounVerbAdj = entry.InnerXml.Split(new string[] { "<fl" }, StringSplitOptions.None)[1].Split(new string[] { "</fl" }, StringSplitOptions.None)[0].Trim()
+                    .Replace("<fw>", "").Replace("</fw>", "").Replace("<sx>", "").Replace("</sx>", "")
+                    .Replace("<vi>", "").Replace("</vi>", "").Replace("<it>", "").Replace("</it>", "")
+                    .Replace(">", "")
+                    .Replace("hindex=\"1\"", "").Replace("hindex=\"2\"", "")
+                    .Replace("hindex=\"3\"", "").Replace("hindex=\"4\"", "")
+                    .Replace("hindex=\"5\"", "").Replace("hindex=\"6\"", "");
+                nounVerbAdj = nounVerbAdj.Split(' ')[0];
+                nounVerbAdj = nounVerbAdj.First().ToString().ToUpper() + nounVerbAdj.Substring(1);
+            }
+            catch (Exception) { nounVerbAdj = "-empty-"; }
+
+            try
+            {
+                defOne = entry.InnerXml.Split(new string[] { "<dt" }, StringSplitOptions.None)[1].Split(new string[] { "</dt" }, StringSplitOptions.None)[0].Trim()
+                    .Replace("<fw>", "").Replace("</fw>", "").Replace("<sx>", "").Replace("</sx>", "")
+                    .Replace("<vi>", "").Replace("</vi>", "").Replace("<it>", "").Replace("</it>", "")
+                    .Replace(">", "")
+                    .Replace("hindex=\"1\"", "").Replace("hindex=\"2\"", "")
+                    .Replace("hindex=\"3\"", "").Replace("hindex=\"4\"", "")
+                    .Replace("hindex=\"5\"", "").Replace("hindex=\"6\"", "");
+                defOne = defOne.First().ToString().ToUpper() + defOne.Substring(1);
+            }
+            catch (Exception) { defOne = "-empty-"; }
+
+            try
+            {
+                defTwo = entry.InnerXml.Split(new string[] { "<dt" }, StringSplitOptions.None)[2].Split(new string[] { "</dt" }, StringSplitOptions.None)[0].Trim()
+                    .Replace("<fw>", "").Replace("</fw>", "").Replace("<sx>", "").Replace("</sx>", "")
+                    .Replace("<vi>", "").Replace("</vi>", "").Replace("<it>", "").Replace("</it>", "")
+                    .Replace(">", "")
+                    .Replace("hindex=\"1\"", "").Replace("hindex=\"2\"", "")
+                    .Replace("hindex=\"3\"", "").Replace("hindex=\"4\"", "")
+                    .Replace("hindex=\"5\"", "").Replace("hindex=\"6\"", "");
+                defTwo = defTwo.First().ToString().ToUpper() + defTwo.Substring(1);
+            }
+            catch (Exception) { defOne = "-empty-"; }
+
+            var wordData = new Tuple<string, string, string, string, string>(word, pronounce, nounVerbAdj, defOne, defTwo);
+
+            return wordData;
+        }
+    }
+}
---
+++ WordOfTheDay/MainPage.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordOfTheDay
+{
+    public partial class MainPage : Form
+    {
+        private Button btnGenerate;
+        private GroupBox gboxWord;
+        private Label lblWord;
+        private Button btnCopy;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainPage));
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.gboxWord = new System.Windows.Forms.GroupBox();
+            this.picLoad = new System.Windows.Forms.PictureBox();
+            this.lblWord = new System.Windows.Forms.Label();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.lblProun = new System.Windows.Forms.Label();
+            this.lblType = new System.Windows.Forms.Label();
+            this.lblDesc1 = new System.Windows.Forms.Label();
+            this.lblDesc2 = new System.Windows.Forms.Label();
+            this.gboxWord.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLoad)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(12, 17);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(81, 30);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // gboxWord
+            // 
+            this.gboxWord.Controls.Add(this.picLoad);
+            this.gboxWord.Controls.Add(this.lblDesc2);
+            this.gboxWord.Controls.Add(this.lblDesc1);
+            this.gboxWord.Controls.Add(this.lblType);
+            this.gboxWord.Controls.Add(this.lblProun);
+            this.gboxWord.Controls.Add(this.lblWord);
+            this.gboxWord.Location = new System.Drawing.Point(12, 60);
+            this.gboxWord.Name = "gboxWord";
+            this.gboxWord.Size = new System.Drawing.Size(368, 278);
+            this.gboxWord.TabIndex = 2;
+            this.gboxWord.TabStop = false;
+            // 
+            // picLoad
+            // 
+            this.picLoad.Image = ((System.Drawing.Image)(resources.GetObject("picLoad.Image")));
+            this.picLoad.Location = new System.Drawing.Point(6, 14);
+            this.picLoad.Name = "picLoad";
+            this.picLoad.Size = new System.Drawing.Size(355, 258);
+            this.picLoad.TabIndex = 1;
+            this.picLoad.TabStop = false;
+            // 
+            // lblWord
+            // 
+            this.lblWord.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWord.Location = new System.Drawing.Point(3, 16);
+            this.lblWord.Name = "lblWord";
+            this.lblWord.Size = new System.Drawing.Size(362, 34);
+            this.lblWord.TabIndex = 0;
+            this.lblWord.Text = "WordGoesHere";
+            this.lblWord.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Enabled = false;
+            this.btnCopy.Location = new System.Drawing.Point(299, 17);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(81, 30);
+            this.btnCopy.TabIndex = 3;
+            this.btnCopy.Text = "&Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            // 
+            // lblProun
+            // 
+            this.lblProun.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblProun.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProun.Location = new System.Drawing.Point(3, 50);
+            this.lblProun.Name = "lblProun";
+            this.lblProun.Size = new System.Drawing.Size(362, 27);
+            this.lblProun.TabIndex = 0;
+            this.lblProun.Text = "PronounceGoesHere";
+            this.lblProun.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblType
+            // 
+            this.lblType.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblType.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblType.Location = new System.Drawing.Point(3, 77);
+            this.lblType.Name = "lblType";
+            this.lblType.Size = new System.Drawing.Size(362, 34);
+            this.lblType.TabIndex = 0;
+            this.lblType.Text = "TypeGoesHere";
+            this.lblType.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblDesc1
+            // 
+            this.lblDesc1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblDesc1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDesc1.Location = new System.Drawing.Point(3, 111);
+            this.lblDesc1.Name = "lblDesc1";
+            this.lblDesc1.Size = new System.Drawing.Size(362, 85);
+            this.lblDesc1.TabIndex = 0;
+            this.lblDesc1.Text = "Description1";
+            this.lblDesc1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblDesc2
+            // 
+            this.lblDesc2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblDesc2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDesc2.Location = new System.Drawing.Point(3, 196);
+            this.lblDesc2.Name = "lblDesc2";
+            this.lblDesc2.Size = new System.Drawing.Size(362, 79);
+            this.lblDesc2.TabIndex = 0;
+            this.lblDesc2.Text = "Description2";
+            this.lblDesc2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // MainPage
+            // 
+            this.AcceptButton = this.btnGenerate;
+            this.ClientSize = new System.Drawing.Size(394, 351);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.gboxWord);
+            this.Controls.Add(this.btnGenerate);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainPage";
+            this.Text = "Word Of The Day";
+            this.Load += new System.EventHandler(this.MainPage_Load);
+            this.gboxWord.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picLoad)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private void MainPage_Load(object sender, EventArgs e)
+        {
+            LoadWord();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            LoadWord();
+        }
+
+        private void LoadWord()
+        {
+            picLoad.Show();
+            picLoad.Update();
+            var newWord = GenerateWordInfo.GetWord();
+            lblWord.Text = newWord.Item1;
+            lblProun.Text = newWord.Item2;
+            lblType.Text = newWord.Item3;
+            lblDesc1.Text = newWord.Item4;
+            lblDesc2.Text = newWord.Item5;
+            picLoad.Hide();
+            picLoad.Update();
+            btnCopy.Enabled = true;
+        }
+    }
+}
